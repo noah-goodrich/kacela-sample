50adc6b0861281bd285867bb8d921087b77167d6
bmidget <benmidget@gmail.com>
2011-07-13 10:16:02 -0600

    Removed unnecessary '|' in menu

--- application/views/template.php
+++ application/views/template.php
@@ -21,13 +21,13 @@
 	<div class="clear"></div>
 		
 	<ul id="navigation">
-		<li><a href="/crud">CRUD</a></li> |
-		<li><a href="/belongsto">Belongs To</a></li> |
-		<li><a href="/hasmany">Has Many</a></li> | 
-		<li><a href="/inheritance_single">Single Table Inheritance</a></li> |
-		<li><a href="/inheritance_concrete">Concrete Table Inheritance</a></li> |
-		<li><a href="/dependents">Dependent Relationships</a></li> |
-		<li><a href="/criteria">Criteria vs Query</a></li> | 
+		<li><a href="/crud">CRUD</a></li>
+		<li><a href="/belongsto">Belongs To</a></li>
+		<li><a href="/hasmany">Has Many</a></li>
+		<li><a href="/inheritance_single">Single Table Inheritance</a></li>
+		<li><a href="/inheritance_concrete">Concrete Table Inheritance</a></li>
+		<li><a href="/dependents">Dependent Relationships</a></li>
+		<li><a href="/criteria">Criteria vs Query</a></li>
 		<li><a href="/debug">Debugging</a></li>
 	</ul>
 	<div class="clear"></div>
